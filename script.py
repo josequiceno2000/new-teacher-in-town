0a02348d5f2a33c2c99ec8d302eb5d0cf1495383
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-02-12 13:04:49 -0600

    Create afterschool combos

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 from roster import student_roster
 from classroom_organizer import ClassroomOrganizer
+import itertools
 
 student_roster_iterator = iter(student_roster)
 
@@ -11,4 +12,14 @@ classroom = ClassroomOrganizer()
 table_combos = classroom.table_combinations()
 
 for combo in table_combos:
+    print(combo)
+
+math_students = classroom.get_students_with_subject("Math")
+science_students = classroom.get_students_with_subject("Science")
+
+math_and_science_students = itertools.chain(math_students, science_students)
+
+afterschool_combos = itertools.combinations(math_and_science_students, 4)
+
+for combo in afterschool_combos:
     print(combo)
